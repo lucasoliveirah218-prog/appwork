3859fd0e2e51e91ad413fe4702084d2078789842
lucasoliveirah218-prog <lucas.oliveirah218@gmail.com>
2026-05-02 22:41:36 -0300

    Delete salvar.js

--- salvar.js
+++
@@ -1,8 +0,0 @@
-export async function onRequestPost(context) {
-  const body = await context.request.json()
-  const id = body.cnpj
-
-  await context.env.MEUBANCO.put(id, JSON.stringify(body))
-
-  return new Response(JSON.stringify({ ok: true }))
-}
